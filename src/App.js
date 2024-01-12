05734370698a99b0ef775455f9814df3f2a9b922
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-01-13 01:35:46 +0430

    Update src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Products from './components/Products';
+
 function App() {
   return (
     <div className="App">
-      <h1>Shopping Cart</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
